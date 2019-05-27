638e62635f107e55435947e873f4786340bfa779
Nicholas Wease <skynet60@outlook.com>
2019-05-27 19:49:46 +0200

    Continued work on sidebar and sidecart.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,12 +10,16 @@ const SideBar = () => {
                 const { links, sideBarOpen, handleSideBar } = value;
 
                 return (
-                    <SideWrapper>
+                    <SideWrapper show={sideBarOpen}>
                         <ul>
                             {links.map(link => {
                                 return (
                                     <li key={link.id}>
-                                        <Link className='sidebar-link' to={link.path}>
+                                        <Link
+                                            className='sidebar-link'
+                                            to={link.path}
+                                            onClick={handleSideBar}
+                                        >
                                             {link.text}
                                         </Link>
                                     </li>
@@ -30,7 +34,42 @@ const SideBar = () => {
 };
 
 const SideWrapper = styled.nav `
-
+  position: fixed;
+  top: 61px;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--mainGray);
+  z-index: 1;
+  border-right: 4px solid var(--primaryColor);
+  transition: var(--mainTransition);
+  transform: ${props => props.show ? 'translateX(0)' : 'translateX(-100%)'};
+  
+  ul {
+    list-style-type: none;
+    padding: 0 !important;
+  }
+  
+  .sidebar-link {
+    display: block;
+    font-size: 1.5rem;
+    text-transform: capitalize;
+    color: var(--mainBlack);
+    padding: 0.5rem 1.5rem;
+    background: transparent;
+    transition: var(--mainTransition);
+  }
+  
+  .sidebar-link:hover {
+    background: var(--primaryColor);
+    color: var(--mainWhite);
+    padding: 0.5rem 1.5rem 0.5rem 2.5rem;
+    text-decoration: none;
+  }
+  
+  @media (min-width: 576px) {
+    width: 20rem;
+  }
 `;
 
 export default SideBar;
