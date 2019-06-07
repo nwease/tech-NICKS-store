c8015ddf017aa302003c8b5a88494ad20f310385
Nicholas Wease <skynet60@outlook.com>
2019-06-07 13:34:08 +0200

    Working with setting up cart totals and lists.

--- src/components/CartSection/CartPillar.jsx
+++ src/components/CartSection/CartPillar.jsx
@@ -2,8 +2,44 @@ import React from 'react';
 
 const CartPillar = () => {
     return (
-        <div>
-            CART PILLAR
+        <div className='container-fluid text-center d-none d-lg-block my-5'>
+            <div className='row'>
+                <div className='col-lg-2'>
+                    <p className='text-uppercase'>
+                        PRODUCT
+                    </p>
+                </div>
+
+                <div className='col-lg-2'>
+                    <p className='text-uppercase'>
+                        NAME
+                    </p>
+                </div>
+
+                <div className='col-lg-2'>
+                    <p className='text-uppercase'>
+                        PRICE
+                    </p>
+                </div>
+
+                <div className='col-lg-2'>
+                    <p className='text-uppercase'>
+                        QUANTITY
+                    </p>
+                </div>
+
+                <div className='col-lg-2'>
+                    <p className='text-uppercase'>
+                        REMOVE
+                    </p>
+                </div>
+
+                <div className='col-lg-2'>
+                    <p className='text-uppercase'>
+                        TOTAL
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/CartSection/CartTotal.jsx
+++ src/components/CartSection/CartTotal.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { ProductConsumer } from '../../context';
 
 const CartTotal = () => {
     return (
-        <div>
-            CART TOTAL
+        <div className='container'>
+            <div className='row'>
+                <ProductConsumer>
+                    {value => {
+                        const {clearCart, cartSubTotal, cartTotal} = value;
+
+                        return (
+                            <div className='col text-title text-center my-4'>
+                                <button className='btn btn-outline-danger text-capitalize mb-4' onClick={clearCart}>
+                                    CLEAR CART
+                                </button>
+
+                                <h3>
+                                    SUBTOTAL: ${cartSubTotal}
+                                </h3>
+                            </div>
+                        )
+                    }}
+                </ProductConsumer>
+            </div>
         </div>
     );
 };
